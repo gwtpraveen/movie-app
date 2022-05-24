4a8afed5ad355f9965d4d140704041bfe5106910
Praveen Thamindu <praveenthamindu@gmail.com>
2022-05-24 15:23:49 +0530

    add styles to search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,11 @@
+import "../style/css/searchbar.css";
+
 const SearchBar = (props) => {
     return ( 
-        <form method="post">
+        <form method="post" className="search">
             <div className="form-group">
-                <img src="./images/icons/icon-search.svg" alt="search icon" />
-                <input type="text" name="userSearch" placeholder="Search from movies or TV series"/>
+                <img src="./images/icons/icon-search.svg" alt="search icon" className="search-icon"/>
+                <input type="text" name="userSearch" placeholder="Search from movies or TV series" className="search-input"/>
             </div>
         </form>
      );
