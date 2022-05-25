68d23ff1ed07655c2efea675256406e7fca3fd37
Praveen Thamindu <praveenthamindu@gmail.com>
2022-05-26 00:45:19 +0530

    change carousels offset from useState to useRef

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,30 +1,33 @@
 import SpecialCard from "./SpecialCard";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import "../style/css/carousel.css";
 
 const Carousel = ({data}) => {
     const carouselLength = useRef(0);
-    const [offset, setOffset] = useState(0);
+    const offset = useRef(0);
+    const card = useRef();
     carouselLength.current =  data.length * 220 * -1;
 
     const moveCarousel = (direction) => {
         if (direction === "right") {
-            if (offset + 220 <= 0) {
-                setOffset(prevVal => prevVal + 220);
+            if (offset.current + 220 <= 0) {
+                offset.current = offset.current + 220;
+                card.current.firstElementChild.style.transform = `translateX(${offset.current}px)`;
             }
         }
         if (direction === "left") {
-            if (offset - 220 > carouselLength.current) {
-                setOffset(prevVal => prevVal - 220);
+            if (offset.current - 220 > carouselLength.current) {
+                offset.current = offset.current - 220;
             } else {
-                setOffset(0);
+                offset.current = 0;
             }
+            card.current.firstElementChild.style.transform = `translateX(${offset.current}px)`;
         }
     }
 
     return ( 
-        <div className="carousel">
-            <div className="container" style={{transform: `translateX(${offset}px)`}}>
+        <div className="carousel" ref={card}>
+            <div className="container">
                 {data.map(item => <SpecialCard data={item} key={item.imdbID}/>)}
             </div>
             <div className="left-arrow arrows" onClick={() => moveCarousel("right")}>
