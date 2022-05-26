36c94e49d9c5dd462a0b656572f0bc2b1fadd02f
Praveen Thamindu <praveenthamindu@gmail.com>
2022-05-27 01:46:48 +0530

    add pagination functionality

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,17 +1,37 @@
 import "../style/css/pagination.css";
-import { useRef } from "react";
+import { useState } from "react";
 
 const Pagination = ({totalItems, itemPerPage}) => {
     const items = totalItems / itemPerPage;
+    const [page, setPage] = useState(1);
     const numbers = [];
+
     for (let i = 0; i < items; i++) {
         numbers.push(i + 1);
     }
+
+    const handlePageNum = (num) => {
+        setPage(num)
+    };
+
+    const clickNext = () => {
+        if (page < items) {
+            setPage(prevVal => prevVal + 1);
+        }
+    };
+
+    const clickPrev = () => {
+        if (page > 1) {
+            setPage(prevVal => prevVal - 1);
+        }
+    };
+
+
     return ( 
         <ul className="pagination">
-            <li className="item prev">Prev</li>
-            {numbers.map(item => <li key={item} className="item">{item}</li>)}
-            <li className="item next">Next</li>
+            <li className="item prev" onClick={clickPrev}>Prev</li>
+            {numbers.map(item => <li key={item} className={page === item ? "item active" : "item"} onClick={() => handlePageNum(item)}>{item}</li>)}
+            <li className="item next" onClick={clickNext}>Next</li>
         </ul>  
      );
 }
