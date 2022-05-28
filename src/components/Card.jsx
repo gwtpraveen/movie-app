ae5d82bc2eace71220643fe7ee76689b5bf2924d
Praveen Thamindu <praveenthamindu@gmail.com>
2022-05-29 00:11:06 +0530

    add intersection observers for lazy loading and animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,9 @@
 import "../style/css/card.css";
-import { useState, memo } from "react";
+import { useState, memo, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({data, onBookmark, bookmarks}) => {
+    const card = useRef()
     const {Title, Year, Rated, Poster, Type, imdbID} = data;
     const [isBookmarked, setIsbookmarked] = useState(bookmarks.includes(imdbID));
 
@@ -10,17 +12,53 @@ const Card = ({data, onBookmark, bookmarks}) => {
         onBookmark(imdbID, Type)
     }
 
+    const opctions = {
+        rootMargin: "0px 0px 0px 0px",
+        threshold: 0.25
+    };
+
+    const lazyLoadingOpctions = {
+        rootMargin: "0px 0px 300px 0px",
+    }
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    card.current.classList.add("show");
+                    observer.unobserve(card.current);
+                }
+            });
+        }, opctions);
+
+        const lazyLoadingObserver = new IntersectionObserver((entries, lazyLoadingObserver) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const imgSrc = Poster;
+                    card.current.firstElementChild.firstElementChild.src = imgSrc;
+                    lazyLoadingObserver.unobserve(card.current);
+                }
+            });
+        }, lazyLoadingOpctions);
+
+        observer.observe(card.current);
+        lazyLoadingObserver.observe(card.current);
+    })
+
+
     return ( 
-         <div className="card">
+         <div className="card" ref={card}>
              <div className="image-container">
-                <img src={Poster} alt="movie poster" className="poster"/>
+                <img src="iWillReplaceSoon" alt="movie poster" className="poster"/>
              </div>
              <div className="details-container">
                  <div className="list">
                      <p className="list-item">{Year}</p>
                      <p className="list-item">{Rated}</p>
                  </div>
-                 <h2 className="title">{Title}</h2>
+                 <Link to={`/home/details/${Type}/${imdbID}`}>
+                    <h2 className="title">{Title}</h2>
+                 </Link>
              </div>
              <div className="bookmark-icon-container" onClick={onClickBookmark}>
                  <img src={`./images/icons/icon-bookmark-${ isBookmarked ? "full":"empty"}.svg`} alt="" className="bookmark-icon"/>
