2d958763a6c5c809667047bc980a17166366b2bb
Praveen Thamindu <praveenthamindu@gmail.com>
2022-06-04 22:12:59 +0530

    add result not found page

---
+++ src/components/ResultNotFound.jsx
@@ -0,0 +1,21 @@
+import "../style/css/resultnotfound.css";
+import movies from "../json/movies.json";
+import Card from "./Card";
+
+const ResultNotFound = ({userSearch, bookmarks, onBookmark}) => {
+    const keywords = userSearch.split(" ");
+    const results = movies.filter(item => item.Title.toLocaleLowerCase().includes(keywords[0].toLocaleLowerCase()))
+    console.log(results)
+    
+    return ( 
+        <section>
+            <a href="/movies">Back</a>
+            <p>result not found on <span>{userSearch}</span></p>
+            <div className="resutlnotfoundMovies">
+                {results.map((item, idx) => <Card key={idx} data={item} bookmarks={bookmarks} onBookmark={onBookmark}/>)}
+            </div>
+        </section>
+     );
+}
+ 
+export default ResultNotFound;
